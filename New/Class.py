e277f1866ef566a1a5940138e038cf623ad6726e
ranjithraja2707 <130454292+ranjithraja2707@users.noreply.github.com>
2023-10-12 18:21:50 +0530

    Add files via upload

--- New/Class.py
+++ New/Class.py
@@ -1,16 +1,22 @@
-class MyClass: #class
-  x = 5
-print(MyClass)
+import random
+def guess_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
 
-class MyClass: # class
-  y = 5
-  x = 10
-p1 = MyClass() #object
-print(p1.y)
-print(p1.x)
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a random number between 1 and 100. Can you guess it?")
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
 
-class raja:
-  def __init__(self,x,y): #using __init__ and also using function(Method) within the class. 
-    self.z = x + y
-my_object = raja(5,10)
-print(my_object.z)
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+guess_number()
